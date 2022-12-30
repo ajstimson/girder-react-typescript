bfb1c1bb9880c7a9a65fcfd33fdef3a80c8efae1
Andrew Stimson <andrew@applejuice.codes>
2022-12-29 21:42:24 -0800

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+    return (
+        <div className="App">
+            <h1>TypeScript App</h1>
+        </div>
+    )
+}
+
+export default App
