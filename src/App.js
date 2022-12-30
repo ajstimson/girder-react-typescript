010644279eed05db099e3145abbaf905ba2f8c74
Andrew Stimson <andrew@applejuice.codes>
2022-12-29 22:26:59 -0800

    Initial Setup: Axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from "./logo.svg"
 import "./App.css"
+import axios from "axios"
+import { useState } from "react"
 
 function App() {
+    const [data, setData] = useState([])
+
+    const getData = async () => {
+        const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/todos"
+        )
+        setData(response.data)
+    }
     return (
         <div className="App">
             <h1>TypeScript App</h1>
+
+            <button onClick={getData}>Get Data</button>
         </div>
     )
 }
